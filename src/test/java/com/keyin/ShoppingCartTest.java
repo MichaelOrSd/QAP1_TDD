efec22f33baa197e631e40d77aa0f17c14eda388
Michael Oreilly <michael.oreilly@keyin.com>
2022-05-30 14:03:51 -0230

    shoppingCartTestFin

--- src/test/java/com/keyin/ShoppingCartTest.java
+++ src/test/java/com/keyin/ShoppingCartTest.java
@@ -17,28 +17,33 @@ public class ShoppingCartTest {
     return new ShoppingCart(Arrays.asList(items));
     } 
 
+    // Testing Cart Total
     @Test 
     public void totalOfEmptyCart() {
         ShoppingCart cart = buildCartWithItems();
         assertEquals(0.0, cart.getTotal(), 0.0);
+        System.out.println("The total should be 0.0 = " + cart.getTotal());
     }
 
     @Test
     public void totalOfOneItem() {
         ShoppingCart cart = buildCartWithItems(new Item(100.0, 1));
         assertEquals(100.0, cart.getTotal(), 0.0);
+        System.out.println("The total should be 100.0 = " + cart.getTotal());
     }
 
     @Test
     public void totalOfTwoItems() {
         ShoppingCart cart = buildCartWithItems(new Item(100.0, 1), (new Item(200.0, 1)));
         assertEquals(300.0, cart.getTotal(), 0.0);
+        System.out.println("The total should be 300.0 = " + cart.getTotal());
     }    
 
     @Test
     public void totalOfItemWithQuanityTwo() {
         ShoppingCart cart = buildCartWithItems(new Item(100.0, 2));
         assertEquals(200.0, cart.getTotal(), 0.0);
+        System.out.println("The total should be 200.0 = " + cart.getTotal());
     }
     
 }
